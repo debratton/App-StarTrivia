c9c8fd811303b9f128a2885ebd9dacbb53afb3eb
debratton <debratton@gmail.com>
2018-10-04 15:27:44 -0500

    Start Project

---
+++ StarTrivia/StarTrivia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarTrivia
+//
+//  Created by David E Bratton on 10/4/18.
+//  Copyright © 2018 David Bratton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
